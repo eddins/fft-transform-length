95af7e812a4cd60a56c782a623b20cd9ac1e0ae8
eddins <4043689+eddins@users.noreply.github.com>
2024-04-24 16:03:32 -0400

    Function implementation and unit tests

--- tests/fftTransformLength_test.m
+++ tests/fftTransformLength_test.m
@@ -1,19 +1,78 @@
 classdef fftTransformLength_test < matlab.unittest.TestCase
 
-    methods (TestClassSetup)
-        % Shared setup for the entire test class
-    end
-
-    methods (TestMethodSetup)
-        % Setup for each test
-    end
-
     methods (Test)
-        % Test methods
+        function alternateMethod(test_case)
+            n = [0:20, 1150:1200];
+            actual = fftTransformLength(n);
+            expected = zeros(size(actual));
+            for k = 1:numel(actual)
+                expected(k) = alternateTransformLengthMethod(n(k));
+            end
 
-        function unimplementedTest(testCase)
-            testCase.verifyTrue(true,"Stub test");
+            test_case.verifyEqual(actual,expected);
+        end
+
+        function elementwiseBehavior(test_case)
+            n = reshape(1:24,2,3,4);
+            actual = fftTransformLength(n);
+            expected = zeros(size(actual),'like',actual);
+            for k = 1:numel(actual)
+                expected(k) = fftTransformLength(n(k));
+            end
+
+            test_case.verifyEqual(actual,expected);
+        end
+
+        function emptyBehavior(test_case)
+            n = zeros(2,3,0,4);
+            actual = fftTransformLength(n);
+            expected = zeros(size(actual),'like',actual);
+            test_case.verifyEqual(actual,expected);
+        end
+
+        function typeBehavior(test_case)
+            np = fftTransformLength(single(23));
+            test_case.verifyClass(np,"single");
+
+            np2 = fftTransformLength(int32(23));
+            test_case.verifyClass(np2,"int32");
+        end
+
+        function overflow(test_case)
+            types = ["uint8" "int8" "uint16" "int16" "uint32" "int32" ...
+                "uint64" "int64"];
+
+            for k = 1:numel(types)
+                test_case.verifyError(...
+                    @() fftTransformLength(intmax(types(k))), ...
+                    "fftTransformLength:overflow")
+            end
+        end
+
+        function inputTooHigh(test_case)
+            test_case.verifyError(...
+                @() fftTransformLength(2*flintmax),...
+                "fftTransformLength:tooHigh");
+        end
+
+        function integerValidation(test_case)
+            test_case.verifyError(...
+                @() fftTransformLength(1.5),...
+                "MATLAB:validators:mustBeInteger");
+        end
+
+        function nonnegativeValidation(test_case)
+            test_case.verifyError(...
+                @() fftTransformLength(-2),...
+                "MATLAB:validators:mustBeNonnegative");            
         end
     end
 
+end
+
+function np = alternateTransformLengthMethod(n)
+    np = n;
+    while (max(factor(np)) > 7)
+        np = np + 1;
+    end
 end
--- toolbox/fftTransformLength.m
+++ toolbox/fftTransformLength.m
@@ -1,3 +1,83 @@
-function B = fftTransformLength(A)
-    B = A;
+%fftTransformLength Optimized length for computing FFT
+%   np = fftTransformLength(n) returns a padded transform length that is
+%   optimized for efficient FFT computation.  Given a nonnegative integer,
+%   n, the output is the smallest integer greater than or equal to n whose
+%   maximum prime factor is 7.  This choice is based on the standard
+%   form of the FFTW library, which computes FFTs most efficiently on
+%   lengths whose prime factors are 2, 3, 5, and 7.
+%
+%   If n is an array, then fftTransformLength operates element-wise,
+%   and np is the same size as n.
+%
+%   fftTransformLength is useful for choosing the padded transform length
+%   in situations where an FFT is zero-padded using syntaxes such as
+%   fft(x,n), fft2(x,m,n), and fftn(x,sz).
+%
+%   EXAMPLE
+%
+%     Compute a two-dimensional convolution using FFTs.
+%
+%       M = 1170;
+%       A = rand(M,M);
+%       N = 101;
+%       B = rand(N,N);
+%
+%       K = M + N - 1;               % convolution length
+%       Kp = fftTransformLength(K);  % zero-padded transform length
+%
+%       Cp = ifft2(fft2(A,Kp,Kp) .* fft2(B,Kp,Kp));
+%       C = Cp(1:K,1:K);
+%
+%   Reference: FFTW 3.3.10 Introduction
+%   (http://www.fftw.org/fftw3_doc/Introduction.html#Introduction)
+%
+%   See also FFT, IFFT, FFT2, IFFT2, FFTN, IFFTN
+
+function np = fftTransformLength(n)
+    arguments
+        n {mustBeInteger, mustBeNonnegative}
+    end
+
+    np = zeros(size(n),'like',n);
+    for k = 1:numel(n)
+        np(k) = padsize(n(k));
+    end
+end
+
+function np = padsize(n)
+    if isfloat(n) && (n > flintmax(class(n)))
+        error("fftTransformLength:tooHigh",...
+            "Input is greater than FLINTMAX.")
+    end
+
+    np = n;
+
+    while true
+        % Divide n evenly, as many times as possible, by the factors 2, 3,
+        % 5, and 7.
+        r = np;
+        for p = [2 3 5 7]
+            while (r > 1) && (mod(r, p) == 0)
+                r = r / p;
+            end
+        end
+
+        % If the result after the above divisions is 1, then we have found
+        % the desired number, so break out of the loop.
+        %
+        % The result can also be 0, if the input was 0. In that case, also
+        % break out of the loop, returning 0.
+        if r <= 1
+            break;
+        else
+            % np has one or more prime factors greater than 7, so try the
+            % next integer.
+            tmp = np + 1;
+            if (tmp == np)
+                error("fftTransformLength:overflow",...
+                    "Integer value too high for the data type.")
+            end
+            np = tmp;
+        end
+    end
 end
